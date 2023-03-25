a94cb5b5ef22380c44df29d0873eea4a260396e3
unknown <mehedi08h@gmail.com>
2023-03-25 16:10:04 +0600

    setup project

---
+++ src/main/java/com/devmehedi/assessment/model/Category.java
@@ -0,0 +1,7 @@
+package com.devmehedi.assessment.model;
+
+public class Category {
+    private Long id;
+    private String title;
+    private String description;
+}
---
+++ src/test/java/com/devmehedi/assessment/AssesmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmehedi.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssesmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
